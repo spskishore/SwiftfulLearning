9f34631b55ce6ccee6799435672f7befe7780407
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-23 18:17:15 +0530

    Initial Commit

---
+++ SwiftfulLearning/SwiftfulLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulLearningApp.swift
+//  SwiftfulLearning
+//
+//  Created by Kishore Kumar on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
