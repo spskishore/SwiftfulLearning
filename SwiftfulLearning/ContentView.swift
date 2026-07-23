c34a01151eeb92e014ec32554882e0fa7bf6f418
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-23 23:28:00 +0530

    Include Button

--- SwiftfulLearning/ContentView.swift
+++ SwiftfulLearning/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking")
+            Button("Click Me") {
+                
+            }
         }
         .padding()
     }
