9f3f33a549dbac58485a216246fb1b2278d470f5
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-23 23:31:20 +0530

    Delete Button

--- SwiftfulLearning/ContentView.swift
+++ SwiftfulLearning/ContentView.swift
@@ -14,9 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking")
-            Button("Click Me") {
-                
-            }
+           
         }
         .padding()
     }
