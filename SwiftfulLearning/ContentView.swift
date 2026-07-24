92e27cbad9883dda83b31049f01c5d75fdc1057b
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-24 21:22:05 +0530

    Update text Ronaldo to Messi

--- SwiftfulLearning/ContentView.swift
+++ SwiftfulLearning/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking")
-            Text("Ronaldo")
+            Text("Messi")
            
         }
         .padding()
