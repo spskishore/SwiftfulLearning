f16a300d5c20488bfc5c669fbbbe56de99035beb
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-24 19:28:13 +0530

    Add new Home View

---
+++ SwiftfulLearning/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulLearning
+//
+//  Created by Kishore Kumar on 24/07/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome Home")
+    }
+}
+
+#Preview {
+    HomeView()
+}
