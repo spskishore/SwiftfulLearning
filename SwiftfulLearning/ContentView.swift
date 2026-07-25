089e045c3941a0d7b08abcbe35863bcec7bd10a5
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-25 18:48:00 +0530

    [Feature]: add text 'welcome aboard'

--- SwiftfulLearning/ContentView.swift
+++ SwiftfulLearning/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking")
-           
+           Text("Welcome Aboard")
         }
         .padding()
         //Rectangle()
