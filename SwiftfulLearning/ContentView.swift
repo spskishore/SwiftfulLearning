31cc9c1a1e73a0de3a2de10bf0b09728f5ed65fd
Kishore Kumar <37957363+spskishore@users.noreply.github.com>
2026-07-25 17:38:50 +0530

    Remove text "Messi and kishore"

--- SwiftfulLearning/ContentView.swift
+++ SwiftfulLearning/ContentView.swift
@@ -14,9 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking")
-            Text("Messi")
-            Text("Kishore")
-            
            
         }
         .padding()
